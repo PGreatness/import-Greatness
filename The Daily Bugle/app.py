609c86693dbca7568cc83d961199caa956439052
Ahnaf Hasan <ahasan1@stuy.edu>
2018-11-24 15:26:34 -0500

    fill in stubs

--- The Daily Bugle/app.py
+++ The Daily Bugle/app.py
@@ -5,17 +5,17 @@ import json, urllib
 app = Flask(__name__)
 
 #stubs for paths to REST APIs
-NEWS_STUB = ""
-WEATHER_STUB = ""
-COMIC_STUB = ""
+NEWS_STUB = "https://developers.nytimes.com/svc/topstories/v2/{}.json?api-key={}" #section of news, api key
+WEATHER_STUB = "https://api.darksky.net/forecast/{}/{},{}" #api key, longitude, latitude
+COMIC_STUB = "http://xkcd.com/{}/info.0.json" #comic number
 
 @app.route('/')
 def home():
     #read json file containing the api keys
-    with open('data/API Keys/keys.json') as json_file:
+    with open('data/API_Keys/keys.json') as json_file:
         json_data = json.loads(json_file.read())
-    print(json_data) #should print           {'News': '', 'Weather': '', 'Comic': ''}
-    print(json_data['News']) # should print      ""
+    print(json_data) #should print           {'News': 'api_key', 'Weather': 'api_key', 'Comic': ''}
+    print(json_data['News']) # should print      "api_key"
 
 
     return render_template('home.html')
