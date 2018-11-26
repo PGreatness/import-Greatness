c6b031553a773f0026cff927c38d9c4d70d9953a
alexliu4 <aliu9@stuy.edu>
2018-11-26 18:45:52 -0500

    fixed readability and added newyorktimes api

--- The Daily Bugle/app.py
+++ The Daily Bugle/app.py
@@ -1,24 +1,31 @@
 from flask import Flask, render_template, request, session, url_for, redirect
-from os import urandom
-import json, urllib
+import json
+import urllib
 
 app = Flask(__name__)
 
-#stubs for paths to REST APIs
-NEWS_STUB = "https://developers.nytimes.com/svc/topstories/v2/{}.json?api-key={}" #section of news, api key
-WEATHER_STUB = "https://api.darksky.net/forecast/{}/{},{}" #api key, longitude, latitude
-COMIC_STUB = "http://xkcd.com/{}/info.0.json" #comic number
+# stubs for paths to REST APIs
+NEWS_STUB = "https://developers.nytimes.com/svc/topstories/v2/{}.json?api-key={}" # section of news, api key
+WEATHER_STUB = "https://api.darksky.net/forecast/{}/{},{}" # api key, longitude, latitude
+COMIC_STUB = "http://xkcd.com/{}/info.0.json" # comic number
 
 @app.route('/')
 def home():
-    #read json file containing the api keys
+    # read json file containing the api keys
     with open('data/API_Keys/keys.json') as json_file:
         json_data = json.loads(json_file.read())
-    print(json_data) #should print           {'News': 'api_key', 'Weather': 'api_key', 'Comic': ''}
-    print(json_data['News']) # should print      "api_key"
+    print(json_data) # should print           {'News': 'api_key', 'Weather': 'api_key', 'Comic': ''}
+    print(json_data['News']) #  should print      "api_key"
 
-    f = urllib.request.urlopen(WEATHER_STUB.format(json_data['Weather'],34,-118)) #34,-118 is LA
-    weather = json.loads(f.read())
+    # # # News API
+    n = urllib.request.urlopen(NEWS_STUB.format("home", json_data['News'])) # 34,-118 is LA
+    news = json.loads(n.read())
+    print ( news["response"]["docs"][0]["headline"]["main"] )
+
+    # # # Weather API
+    w = urllib.request.urlopen(WEATHER_STUB.format(json_data['Weather'], 34, -118)) # 34,-118 is LA
+    weather = json.loads(w.read())
+
     return render_template('home.html')
 
 
@@ -28,20 +35,21 @@ def login():
 
 @app.route('/auth')
 def auth():
-    ###Authenticate
+    # # # Authenticate
     username_input = request.form.get("username")
     password_input = request.form.get("password")
-    all_usernames = dict() #FILL IN
+    all_usernames = dict() # FILL IN
     if username_input in all_usernames:
-        hashed_pass = password_input #FILL IN 
+        hashed_pass = password_input # FILL IN
         if hashed_pass == all_usernames[username_input]:
-            #Log them in
+            # Log them in
             pass
-        #Failed password and username match
+        # Failed password and username match
         pass
-    #Username doesnt exist
+    # Username doesnt exist
 
     return redirect(url_for(home))
+
 
 if __name__ == "__main__":
     app.debug = True
