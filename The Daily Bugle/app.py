a81d16ba801c2c28178c8e67852aeb68450c8886
alexliu4 <aliu9@stuy.edu>
2018-11-27 13:03:31 -0500

    fixed the issue with the NEWS_STUB

--- The Daily Bugle/app.py
+++ The Daily Bugle/app.py
@@ -6,7 +6,7 @@ import ssl
 app = Flask(__name__)
 
 # stubs for paths to REST APIs
-NEWS_STUB = "https://developers.nytimes.com/svc/topstories/v2/{}.json?api-key={}" # section of news, api key
+NEWS_STUB = "https://api.nytimes.com/svc/topstories/v2/{}.json?api-key={}" # section of news, api key
 WEATHER_STUB = "https://api.darksky.net/forecast/{}/{},{}" # api key, longitude, latitude
 COMIC_STUB = "http://xkcd.com/{}/info.0.json" # comic number
 
@@ -21,11 +21,11 @@ def home():
     print(json_data) # should print           {'News': 'api_key', 'Weather': 'api_key', 'Comic': ''}
     print(json_data['News']) #  should print      "api_key"
 
-    # # # # News API
-    #
-    # n = urllib.request.urlopen(NEWS_STUB.format("home", json_data['News']))
-    # news = json.loads(n.read())
-    # print ( news["response"]["docs"][0]["headline"]["main"] )
+    # # # News API
+
+    n = urllib.request.urlopen(NEWS_STUB.format("home", json_data['News']), context=context)
+    news = json.loads(n.read())
+    # print ( news )
 
     # # # Weather API
     w = urllib.request.urlopen(WEATHER_STUB.format(json_data['Weather'], 34, -118), context=context) # 34,-118 is LA
